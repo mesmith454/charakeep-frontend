2d0ce9a7222cc138bcaa7e847bbe0dd603db74c6
mesmith454 <mesmith454@gmail.com>
2021-03-09 15:58:50 -0800

    def vars and pscode js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//async defer set up in index
+//define variables 
+    const logbutton = document.querySelector("#buttolog")
+    const addbutton = document.querySelector("#add")
+    const editbutton = document.querySelector("#edit")
+    const delbutton = document.querySelector("#delete")
+    const image = document.querySelector("#profile")
+    const 
+
+//stand alone functions
+  //login function - logbutton accepts a name and loads an existing user and their characters OR 
+  //create new user-creates a user and renders a create character sheet > add button in this case should 
+  //add character - toggle view to new character form populated with labels> add button now reads save 
+  //save character - record to database and render saved character information 
+  //edit character - toggle to edit rendered view to form populated with char's info > button text to update
+  //update character - record info over existing info for current character to database
+  //delete - changes inner text to confirm
+  //confdelete - removes char from db
+
+//event listeners
+  //login onclick > login/create function
+  //add - on click > add function
+  //add>save - on click > save function
+  //edit - on click > edit function
+  //edit>update - on click > update function
+  //delete - on click > delete function
+  //delete>update - on click > confdelete function 
+
+//call functions
