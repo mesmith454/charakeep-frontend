11593eecd074076f26c9cd4ecae17af107ede0a1
mesmith454 <mesmith454@gmail.com>
2021-03-10 16:37:06 -0800

    crying over js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,78 @@
 //async defer set up in index script tags
 
 //define variables 
-    const logbutton = document.querySelector("#buttolog")
+    const logbutton = document.querySelector("#logbtn")
     const addbutton = document.querySelector("#add")
     const editbutton = document.querySelector("#edit")
     const delbutton = document.querySelector("#delete")
     const image = document.querySelector("#profile")
-    // const each die will need to be defined for rollers 'd#equals'. maybe do this as an array i can iterate thru
     const buttchieve = document.querySelector("#buttchieve")
+    conts 
+
+    let li = document.createElement(li)
 
 //stand alone functions
- 
+  // function fetch('http://localhost:3000', {method: 'GET'}) {
+  // .then(db => db.json())
+  //   .then(db.json => console.log(db.json))
+  // } //this doesn't seem right 
+
   function login(string){
     //if User.all.includes?(string)
-        //render json: user's 1st character
-    //else creates a user and renders a create character sheet
+        //render json: User.character.first
+    //else User.create(name: string)
+      //render json: form.html view
   }
   
   function addchar() {
-    //add - toggle view to new character form populated with labels> add button now reads save 
+    //add - render view to new character form populated with labels> add button innerText to 'save'
   }
 
   function save() {
-  //save character - record to database and render saved character information 
+    //save character - fetch() to database and render saved character information 
   }
 
   function edit(character) { 
-    //load form view and populate fields with character info
+    //render form view and populate fields with character info via fetch
   }
 
   function update(character) {
-
+    //fetch character
+    //record input to character
+    //fetch() return updated character
   }
-  //edit character - toggle to edit rendered view to form populated with char's info > button text to update
-  //update character - record info over existing info for current character to database
-
-  //delete - changes inner text to confirm
-  //confdelete - removes char from db
+  
+  function deletechar(character) {
+    //fetch character
+    //delete character from database
+    //if exist other characters display 1st
+    //else display form for new 
+  }
 
   //die rolls - each die will need a function to:
+
+  function rolldie(dnumber, amount){
     //take into acount die range
     //take in amount of dice to be rolled (we'll say max 10 each)
     //clicking = will calculate result
     //interpolate die number to keep it dry
+  }
 
-  //buttchieve - append input in textfield to ul as li above the textfield. Do i need to div those?
+
 
 //event listeners
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  let input = e.target[0].value
+  let li = document.createElement('li')
+  li.innerText = input
+  ul.appendChild(li)
+
+  input.innerText = ''
+});
   //login - on click > login/create function
+    
   //add - on click > add function
   //add>save - on click > save function
   //edit - on click > edit function
