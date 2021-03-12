43f14e71efaf9f8009d7655febdf17d0bab35b21
mesmith454 <mesmith454@gmail.com>
2021-03-11 21:48:54 -0800

    hm

--- src/index.js
+++ src/index.js
@@ -1,85 +1,96 @@
-//async defer set up in index script tags
-
 //define variables 
-    const logbutton = document.querySelector("#logbtn")
-    const addbutton = document.querySelector("#add")
-    const editbutton = document.querySelector("#edit")
-    const delbutton = document.querySelector("#delete")
+    const logbtn = document.querySelector("#logbtn")
+    const addbtn = document.querySelector("#add")
+    const editbtn = document.querySelector("#edit")
+    const delbtn = document.querySelector("#delete")
     const image = document.querySelector("#profile")
-    const buttchieve = document.querySelector("#buttchieve")
-    conts 
+    const addchieve = document.querySelector("#addchieve")
+    const usern = document.querySelector("#usern")
+  
     const achul = document.querySelector("achul")
 
-    let li = document.createElement(li)
+    let li = document.createElement("li")
 
 //stand alone functions
-  // function fetch('http://localhost:3000', {method: 'GET'}) {
-  // .then(db => db.json())
-  //   .then(db.json => console.log(db.json))
-  // } //this doesn't seem right 
+  function getuser(){
+    fetch('http://localhost:3000/users/1', {method: 'GET'}) 
+    .then(db => db.json())
+    .then(user => console.log(user))
+   } 
+   //populate with user and char data
 
-  function login(string){
-    //if User.all.includes?(string)
-        //render json: User.character.first
-    //else User.create(name: string)
-      //render json: form.html view
-  }
+  function getallcharas() {   
+    fetch('http://localhost:3000/characters', {method: 'GET'}) 
+    .then(db => db.json())
+    .then(character => console.log(character))
+   } //this works yay!
+
+   function getchara() {   
+    fetch('http://localhost:3000/characters/:id', {method: 'GET'}) 
+    .then(db => db.json())
+    .then(character => console.log(character))
+   } 
+//event listeners
+  // addach.addEventListener("click", e => {
+    // e.preventDefault()
+    // let input = e.target[0].value
+    // let li = document.createElement('li')
+    // li.innerText = input
+    // achul.appendChild('li')
+    // input.innerText = ''
+  // });
+    
+  // logbtn.addEventListener("click", e =>{
+  //   e.preventDefault()
+  //   if (db.include?()) {
+  //     getuser();
+  //   } else {
+  //     User.create(name: usern)
+  //   } 
+  // }) this can wait
+    
+  //addbtn.addEventListener("click", e => {
+    //render form.html
+    //input = []
+        //toggle form view, accept form input as array
+
+    //async function addChar(url = 'http://localhost:3000/characters', data = {}) {
+      //method: 'POST',           //post submission to db
+      //mode: 'cors',
+      //headers: {
+        //'Content-Type': 'application/json'
+      //},
+      //redirect: 'follow',
+      //referrePolicy: 'no-referrer',
+      //body: JSON.stringify(data);
+    //}
+  //});
+
+   
+
+  //edit - on click > edit function
+    //render form.html
   
-  function addchar() {
-    //add - render view to new character form populated with labels> add button innerText to 'save'
-  }
+  //edit>update - on click > update function
+      //edit current character 
+      //record input to character
+      //fetch() return updated character
 
-  function save() {
-    //save character - fetch() to database and render saved character information 
-  }
+  //delete - on click > delete function
+      //fetch character
+      //delete character from database
+      //if exist other characters display 1st
+      //else display form for new 
 
-  function edit(character) { 
-    //render form view and populate fields with character info via fetch
-  }
-
-  function update(character) {
-    //fetch character
-    //record input to character
-    //fetch() return updated character
-  }
-  
-  function deletechar(character) {
-    //fetch character
-    //delete character from database
-    //if exist other characters display 1st
-    //else display form for new 
-  }
-
-  //die rolls - each die will need a function to:
-
-  function rolldie(dnumber, amount){
-    if dnumber 
-    //take in amount of dice to be rolled (we'll say max 10 each)
+  //dieroller - on click 
+    //if id = d#; n = 
+    //take input for amount
+    // dn x amount
     //clicking = will calculate result
     //interpolate die number to keep it dry
-  }
+  
 
-
-
-//event listeners
-
-addachform.addEventListener('submit', e => {
-  e.preventDefault()
-  let input = e.target[0].value
-  let li = document.createElement('li')
-  li.innerText = input
-  achul.appendChild(li)
-
-  input.innerText = ''
-});
-  //login - on click > login/create function
-    
-  //add - on click > add function
-  //add>save - on click > save function
-  //edit - on click > edit function
-  //edit>update - on click > update function
-  //delete - on click > delete function
-  //delete>update - on click > confdelete function 
-  //
 
 //call functions
+getuser();
+// getallcharas();
